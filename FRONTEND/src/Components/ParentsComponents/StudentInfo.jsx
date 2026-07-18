3d5f8244281a2191655aeea9d184c8960a34182d
unknown <darren.guansing@cvsu.edu.ph>
2026-07-19 00:34:05 +0800

    undone

--- FRONTEND/src/Components/ParentsComponents/StudentInfo.jsx
+++ FRONTEND/src/Components/ParentsComponents/StudentInfo.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const StudentInfo = ({Htittle, Otittle }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-2">
-      <div className="w-fit rounded-lg bg-swamp-green/30 px-4 py-5 text-center shadow-sm">
+      <div className="w-fit rounded-lg bg-white/40 px-4 py-5 text-center shadow-sm">
         <span className="text-sm sm:text-base font-semibold text-egg-dark whitespace-nowrap">{Otittle}</span>
       </div>
-      <span className="text-xs sm:text-sm font-medium text-black text-center whitespace-nowrap">[ {Htittle} ]</span>
+      <span className="text-xs sm:text-sm font-medium text-white text-center whitespace-nowrap"> {Htittle} </span>
     </div>
   )
 }
--- FRONTEND/src/Home/ParentsDashboard.jsx
+++ FRONTEND/src/Home/ParentsDashboard.jsx
@@ -6,19 +6,19 @@ const ParentsDashboard = () => {
   return (
     <div className="flex flex-col gap-6 p-4 sm:p-6 font-[Poppins] cursor-default">
       <div className="flex justify-center">
-        <div className="flex flex-col gap-4 border border-swamp-green rounded-lg bg-bone p-4 sm:p-6 w-full max-w-4xl">
+        <div className="flex flex-col gap-4 border border-swamp-green rounded-lg bg-green-900 p-4 sm:p-6 w-full max-w-4xl">
           <div className="flex flex-col sm:flex-row items-center gap-3 sm:gap-4 text-center sm:text-left">
-            <img
+            {/* <img
               src="/assets/logo.png"
               alt="logo"
-              className="w-16 sm:w-20 rounded-full object-cover shrink-0"
-            />
-            <span className="text-3xl sm:text-2xl md:text-4xl font-[PoppinsBold] text-swamp-green hover:text-2xl duration-300">
+              className="w-16 h-20 object-contain drop-shadow-md sm:w-20 rounded-full shrink-0"
+            /> */}
+            <span className="text-3xl p-3 sm:text-2xl md:text-4xl font-[PoppinsBold] text-white">
               Bautista, Sofia Anne
             </span>
           </div>
 
-          <div className="flex flex-wrap justify-center gap-4 md:gap-8 lg:gap-5 p-4 sm:p-6 border border-swamp-green rounded-lg bg-bone">
+          <div className="flex flex-wrap justify-center gap-4 md:gap-8 lg:gap-5 p-4 sm:p-6 border border-swamp-green rounded-lg">
             <StudentInfo Htittle="LRN" Otittle="2026147528128" />
             <StudentInfo Htittle="Class Schedule" Otittle="Monday - Friday" />
             <StudentInfo Htittle="Class Time" Otittle="7am - 10am" />
